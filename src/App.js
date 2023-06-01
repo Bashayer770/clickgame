40bb19d1417e82d243e2e5e86da68caf3e8fc255
bashayer770 <beshow95@gmail.com>
2023-06-01 17:26:42 +0300

    click

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import "./App.css";
 import Header from "./components/Header";
 import { Clickpage } from "./components/Clickpage";
 import { useState } from "react";
-import Sniper from "./Sounds/Sniper.mp3";
+import boop from "./Sounds/boop.mp3";
 import funtown from "./Sounds/funtown.mp3";
+import collecting from "./Sounds/collecting.mp3";
+import achievement from "./Sounds/achievement.mp3";
 
 function App() {
   const [Num, setNum] = useState(1);
   const [coin, setCoin] = useState(Num);
+  const [clicks, setClicks] = useState(2);
+  const [theupgrade, setTheupgrade] = useState(false);
 
   function building() {
     setCoin(coin - 3);
   }
 
   function clicked() {
-    new Audio(Sniper).play();
+    new Audio(boop).play();
     setNum(Num + 1);
     setCoin(coin + 1);
+    new Audio(collecting).play();
   }
 
+  function double() {
+    setClicks(clicks + 2);
+  }
   function upgrade() {
-    new Audio(funtown).play();
-    setNum((Num) => Num * 6);
+    new Audio(achievement).play();
     setCoin(coin - 3);
+    setNum(Num + 2);
   }
 
   //function play(){
@@ -34,9 +42,9 @@ function App() {
     <div id="demo" className="swap">
       <Header />
       <h4>{coin} coins</h4>
-      <h3>{Num}</h3>
+      <h3 className="score">{Num}</h3>
 
-      <h6>{Num}destroy planets</h6>
+      <h6>{Num} destroyed planets</h6>
       <button
         onClick={clicked}
         className={`buttonDestroy ${Num % 5 == 0 ? "rotateme" : ""}`}
